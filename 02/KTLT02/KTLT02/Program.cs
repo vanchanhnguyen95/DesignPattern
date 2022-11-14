1c5f3ca05f30156ac8562b2d08232d7974b2e092
DESKTOP-T0HLH7K\os <vanchanhnguyen95@gmail.com>
2022-11-14 15:09:31 +0700

    add KTLT (C#) ôn tập C5

---
+++ 02/KTLT02/KTLT02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace KTLT02
+{
+    class Program
+    {
+        /*
+         Khác nhau while, do-while, for
+        while: dùng white khi không biết sẽ lặp bao nhiêu lần,  kiểm tra trước rồi lặp sau
+        do-while:  dùng do-white khi không biết sẽ lặp bao nhiêu lần, lặp trước kiểm tra sau
+        for: dùng for khi biết sẽ lặp bao nhiêu lần
+
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
